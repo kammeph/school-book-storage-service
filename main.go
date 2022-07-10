fbad3f54d054072c4a1fbadfa3627b7ed52c21c9
kammeph <ph.kammerer@googlemail.com>
2022-07-10 22:34:54 +0200

    use postgres store

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/kammeph/school-book-storage-service/infrastructure/dbs"
 	"github.com/kammeph/school-book-storage-service/web/storage"
 )
 
 func main() {
-	storage.ConfigureEndpointsWithMemoryStore()
+	db := dbs.NewPostgresDB()
+	defer db.Close()
+	storage.ConfigureEndpointWithPostgresStore(db)
+	// storage.ConfigureEndpointsWithMemoryStore()
 	http.ListenAndServe(":9090", nil)
 }
