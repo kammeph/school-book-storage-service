e697a214ddc90e1ed1181dbbfcab0b82b38399c1
kammeph <ph.kammerer@googlemail.com>
2022-07-16 21:53:24 +0200

    extend model by on method and add load and apply method to aggregate interface

--- domain/common/aggregate.go
+++ domain/common/aggregate.go
@@ -1,9 +1,16 @@
 package common
 
+type on func(event Event) error
+
 type AggregateModel struct {
 	ID      string
 	Version int
 	Events  []Event
+	on      on
+}
+
+func NewAggregateModel(on on) AggregateModel {
+	return AggregateModel{on: on, Events: []Event{}}
 }
 
 func (a AggregateModel) AggregateID() string {
@@ -22,10 +29,31 @@ func (a AggregateModel) DomainEvents() []Event {
 	return a.Events
 }
 
+func (a AggregateModel) Load(events []Event) error {
+	for _, event := range events {
+		err := a.on(event)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (a AggregateModel) Apply(event Event) error {
+	err := a.on(event)
+	if err != nil {
+		return err
+	}
+	a.Events = append(a.Events, event)
+	return nil
+}
+
 type Aggregate interface {
 	AggregateID() string
 	SetAggregateID(string)
 	AggregateVersion() int
 	DomainEvents() []Event
+	Load(events []Event) error
+	Apply(event Event) error
 	On(event Event) error
 }
