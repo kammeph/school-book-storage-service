5e5a5433d71db789a08b3754bc99856ef0891f2d
kammeph <ph.kammerer@googlemail.com>
2022-07-16 21:52:23 +0200

    extend event model by type and data

--- domain/common/event.go
+++ domain/common/event.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"encoding/json"
 	"time"
 )
 
@@ -8,16 +9,27 @@ type Event interface {
 	AggregateID() string
 	EventVersion() int
 	EventAt() time.Time
-}
-
-type EventTyper interface {
 	EventType() string
+	EventData() string
+	GetJsonData(data interface{}) error
+	SetJsonData(data interface{}) error
 }
 
 type EventModel struct {
 	ID      string
 	Version int
 	At      time.Time
+	Type    string
+	Data    string
+}
+
+func NewEvent(aggregate Aggregate, eventType string) Event {
+	return EventModel{
+		ID:      aggregate.AggregateID(),
+		Version: aggregate.AggregateVersion() + 1,
+		At:      time.Now(),
+		Type:    eventType,
+	}
 }
 
 func (m EventModel) AggregateID() string {
@@ -30,4 +42,25 @@ func (m EventModel) EventVersion() int {
 
 func (m EventModel) EventAt() time.Time {
 	return m.At
+}
+
+func (m EventModel) EventType() string {
+	return m.Type
+}
+
+func (m EventModel) EventData() string {
+	return m.Data
+}
+
+func (m EventModel) GetJsonData(data interface{}) error {
+	return json.Unmarshal([]byte(m.Data), data)
+}
+
+func (m EventModel) SetJsonData(data interface{}) error {
+	eventData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	m.Data = string(eventData)
+	return nil
 }
