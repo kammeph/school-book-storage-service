0b08b5e93c15579c88e8fd8470a42bcd75ca556f
kammeph <ph.kammerer@googlemail.com>
2022-07-17 22:51:23 +0200

    move sorting of events to domain

--- application/common/store.go
+++ application/common/store.go
@@ -6,24 +6,6 @@ import (
 	"github.com/kammeph/school-book-storage-service/domain/common"
 )
 
-// History represents
-type History []common.Event
-
-// Len implements sort.Interface
-func (h History) Len() int {
-	return len(h)
-}
-
-// Swap implements sort.Interface
-func (h History) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
-
-// Less implements sort.Interface
-func (h History) Less(i, j int) bool {
-	return h[i].EventVersion() < h[j].EventVersion()
-}
-
 type Store interface {
 	Load(ctx context.Context, aggregate common.Aggregate) error
 	Save(ctx context.Context, aggregate common.Aggregate) error
