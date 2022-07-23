4e0b74cbc4349f3a50d1214fd2afa061d1210fda
kammeph <ph.kammerer@googlemail.com>
2022-07-23 19:11:53 +0200

    refactor storage commands test

--- application/storage/commands_test.go
+++ application/storage/commands_test.go
@@ -2,25 +2,51 @@ package storage_test
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
+	"time"
 
-	"github.com/google/uuid"
 	"github.com/kammeph/school-book-storage-service/application/common"
 	"github.com/kammeph/school-book-storage-service/application/storage"
 	domain_common "github.com/kammeph/school-book-storage-service/domain/common"
+	domain_storage "github.com/kammeph/school-book-storage-service/domain/storage"
 	"github.com/stretchr/testify/assert"
 )
 
-type EntityAggregate struct {
-	domain_common.AggregateModel
-}
-
-func (e *EntityAggregate) On(event domain_common.Event) error {
-	return nil
-}
-
 type memoryStore struct {
 	eventsById map[string][]domain_common.Event
+}
+
+func newMemoryStoreWithDefaultEvents() *memoryStore {
+	store := &memoryStore{eventsById: map[string][]domain_common.Event{}}
+	eventDataForRemove, _ := json.Marshal(domain_storage.StorageAddedEvent{
+		SchoolID:  "school",
+		StorageID: "testRemove",
+		Name:      "Closet to Remove",
+		Location:  "Room 12",
+	})
+	eventForRemove := domain_common.EventModel{
+		ID:      "school",
+		Type:    domain_storage.StorageAdded,
+		Version: 1,
+		At:      time.Now(),
+		Data:    string(eventDataForRemove),
+	}
+	eventDataForUpdate, _ := json.Marshal(domain_storage.StorageAddedEvent{
+		SchoolID:  "school",
+		StorageID: "testUpdate",
+		Name:      "Closet to Update",
+		Location:  "Room 12",
+	})
+	eventForUpdate := domain_common.EventModel{
+		ID:      "school",
+		Type:    domain_storage.StorageAdded,
+		Version: 2,
+		At:      time.Now(),
+		Data:    string(eventDataForUpdate),
+	}
+	store.eventsById["school"] = []domain_common.Event{&eventForRemove, &eventForUpdate}
+	return store
 }
 
 func (s *memoryStore) Save(ctx context.Context, aggregate domain_common.Aggregate) error {
@@ -43,60 +69,47 @@ func (s *memoryStore) Load(ctx context.Context, aggregate domain_common.Aggregat
 	return nil
 }
 
-var store = &memoryStore{eventsById: map[string][]domain_common.Event{}}
+var store = newMemoryStoreWithDefaultEvents()
 
 func TestHandleAddStorage(t *testing.T) {
-	handler := storage.NewAddStorageCommandHandler(store, nil)
-	commandId := uuid.New().String()
 	ctx := context.Background()
-	add := storage.AddStorageCommand{CommandModel: common.CommandModel{ID: commandId}, Name: "storage", Location: "location"}
-	storageID, err := handler.Handle(ctx, add)
+	handler := storage.NewAddStorageCommandHandler(store, nil)
+	command := storage.AddStorageCommand{CommandModel: common.CommandModel{ID: "school"}, Name: "storage", Location: "location"}
+	storageID, err := handler.Handle(ctx, command)
 	assert.Nil(t, err)
 	assert.NotZero(t, storageID, 3)
 }
 
 func TestHandleRemoveStorage(t *testing.T) {
-	addHandler := storage.NewAddStorageCommandHandler(store, nil)
-	commandId := uuid.New().String()
 	ctx := context.Background()
-	add := storage.AddStorageCommand{CommandModel: common.CommandModel{ID: commandId}, Name: "storage", Location: "location"}
-	storageID, err := addHandler.Handle(ctx, add)
 	removeHandler := storage.NewRemoveStorageCommandHandler(store, nil)
-	remove := storage.RemoveStorageCommand{CommandModel: common.CommandModel{ID: commandId}, StorageID: storageID, Reason: "test"}
-	err = removeHandler.Handle(ctx, remove)
+	remove := storage.RemoveStorageCommand{CommandModel: common.CommandModel{ID: "school"}, StorageID: "testRemove", Reason: "test"}
+	err := removeHandler.Handle(ctx, remove)
 	assert.Nil(t, err)
 }
 
 func TestHandleSetStorageName(t *testing.T) {
-	addHandler := storage.NewAddStorageCommandHandler(store, nil)
-	commandId := uuid.New().String()
 	ctx := context.Background()
-	add := storage.AddStorageCommand{CommandModel: common.CommandModel{ID: commandId}, Name: "storage", Location: "location"}
-	storageID, err := addHandler.Handle(ctx, add)
-	setNameHandler := storage.NewRenameStorageCommandHandler(store, nil)
-	setName := storage.RenameStorageCommand{
-		CommandModel: common.CommandModel{ID: commandId},
-		StorageID:    storageID,
+	handler := storage.NewRenameStorageCommandHandler(store, nil)
+	command := storage.RenameStorageCommand{
+		CommandModel: common.CommandModel{ID: "school"},
+		StorageID:    "testUpdate",
 		Name:         "storage name set",
 		Reason:       "test",
 	}
-	err = setNameHandler.Handle(ctx, setName)
+	err := handler.Handle(ctx, command)
 	assert.Nil(t, err)
 }
 
 func TestHandleSetStorageLocation(t *testing.T) {
-	addHandler := storage.NewAddStorageCommandHandler(store, nil)
-	commandId := uuid.New().String()
 	ctx := context.Background()
-	add := storage.AddStorageCommand{CommandModel: common.CommandModel{ID: commandId}, Name: "storage", Location: "location"}
-	dto, err := addHandler.Handle(ctx, add)
-	setLocationHandler := storage.NewRelocateStorageCommandHandler(store, nil)
-	setLocation := storage.RelocateStorageCommand{
-		CommandModel: common.CommandModel{ID: commandId},
-		StorageID:    dto,
+	handler := storage.NewRelocateStorageCommandHandler(store, nil)
+	command := storage.RelocateStorageCommand{
+		CommandModel: common.CommandModel{ID: "school"},
+		StorageID:    "testUpdate",
 		Location:     "location set",
 		Reason:       "test",
 	}
-	err = setLocationHandler.Handle(ctx, setLocation)
+	err := handler.Handle(ctx, command)
 	assert.Nil(t, err)
 }
