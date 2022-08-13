93dbc2f0720835ae049ff4f18f51c788420d6118
kammeph <ph.kammerer@googlemail.com>
2022-08-13 23:27:51 +0200

    chore: removed database init go file

--- web/cmd/main.go
+++ web/cmd/main.go
@@ -14,7 +14,6 @@ import (
 )
 
 func main() {
-	// postgres.CreatePostgresStoreTables()
 	connection := rabbitmq.NewRabbitMQConnection()
 	defer func() {
 		if err := connection.Close(); err != nil {
