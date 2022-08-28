9f41be5f1961cb7a3ce3cd6a7cb7b2f87c17a2b7
kammeph <ph.kammerer@googlemail.com>
2022-08-28 20:48:56 +0200

    feat: added cors middleware function

--- web/requests.go
+++ web/requests.go
@@ -1,23 +1,41 @@
 package web
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
+
+var corsAllowOrigin = os.Getenv("CORS_ALLOW_ORIGIN")
+
+func setupCORS(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", corsAllowOrigin)
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func setContentTypeJson(w *http.ResponseWriter) {
+	(*w).Header().Set("Content-Type", "application/json")
+}
 
 func Get(pattern string, handler http.HandlerFunc) {
-	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-		handler(w, r)
-	})
+	Request(pattern, handler, http.MethodGet)
 }
 
 func Post(pattern string, handler http.HandlerFunc) {
+	Request(pattern, handler, http.MethodPost)
+}
+
+func Request(pattern string, handler http.HandlerFunc, method string) {
 	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
+		setContentTypeJson(&w)
+		setupCORS(&w)
+		if r.Method == method {
+			handler(w, r)
 			return
 		}
-		handler(w, r)
+		if r.Method != http.MethodOptions {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
 	})
 }
